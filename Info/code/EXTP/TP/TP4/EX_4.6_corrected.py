1a932ce3a2f5bd6102f2f7bb4605b956eee322ec
Serge RAVEZ <serge.ravez@laposte.net>
2025-10-08 20:31:37 +0200

    Update EX_4.6_corrected.py

--- Info/code/EXTP/TP/TP4/EX_4.6_corrected.py
+++ Info/code/EXTP/TP/TP4/EX_4.6_corrected.py
@@ -1,14 +1,31 @@
+def manage_zeros(a: int, b:int):
+    if a * b == 0:
+        if a + b == 0:
+            return -1
+        else:
+            return max(a,b)
+    else:
+        return 0
+
+
 def pgcd_par_diviseurs(a: int,b: int)-> int:
     '''Cherche le plus grand diviseur commun de manière
     décroissante en commençant par b'''
     a = abs(a)
     b = abs(b)
-    i = min(a,b)
-    while i>1:
-        if a%i == 0 and b%i == 0:
-            break
-        i -= 1
-    return i
+    if a * b == 0:
+        if a + b == 0:
+            # pgcd(0,0) n'existe pas
+            return 0
+        else:
+            return max(a,b)
+    else:
+        i = min(a,b)
+        while i>1:
+            if a%i == 0 and b%i == 0:
+                break
+            i -= 1
+        return i
 
 def pgcd_par_differences(a: int,b: int)-> int:
     '''calcule le pgcd par l algorithme des differences :
@@ -32,29 +49,34 @@ def pgcd_par_euclide(a,b):
     et réitérer'''
     a = abs(a)
     b = abs(b)
-    r = a % b
-    while r != 0:
-        a = b
-        b = r
+    z = manage_zeros(a,b)
+    if a * b == 0:
+        if a + b == 0:
+            # pgcd(0,0) n'existe pas
+            return 0
+        else:
+            return max(a,b)
+    else:
         r = a % b
-    return b
+        while r != 0:
+            a = b
+            b = r
+            r = a % b
+        return b
 
-print(pgcd_par_diviseurs(36,15))
-print(pgcd_par_differences(36,15))
-print(pgcd_par_euclide(36,15))
+tests = [
+    [36,15,3],
+    [40,20, 20],
+    [13,7,1],
+    [4,0,4],
+    [0,0,0]
+]
 
-print(pgcd_par_diviseurs(15,36))
-print(pgcd_par_differences(15,36))
-print(pgcd_par_euclide(15,36))
-
-print(pgcd_par_diviseurs(40,20))
-print(pgcd_par_differences(40,20))
-print(pgcd_par_euclide(40,20))
-
-print(pgcd_par_diviseurs(13,7))
-print(pgcd_par_differences(13,7))
-print(pgcd_par_euclide(13,7))
-
-print(pgcd_par_diviseurs(4,0))
-print(pgcd_par_differences(4,0))
-print(pgcd_par_euclide(4,0))
+for t in tests:
+    print(f"pgcd({t[0]},{t[1]}) = {t[2]}")
+    print( "    div     :", pgcd_par_diviseurs(t[0],t[1])== t[2])
+    print( "    div  (r):", pgcd_par_diviseurs(t[1],t[0])== t[2])
+    print( "    diff    :", pgcd_par_differences(t[0],t[1])== t[2])
+    print( "    diff (r):", pgcd_par_differences(t[1],t[0])== t[2])
+    print( "    eucl    :", pgcd_par_euclide(t[0],t[1])== t[2])
+    print( "    eucl (r):", pgcd_par_euclide(t[1],t[0])== t[2])
