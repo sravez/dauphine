4aa554e97870ed48f9083e223e72f6099b1e2cd5
Serge RAVEZ <serge.ravez@laposte.net>
2025-10-10 16:51:15 +0200

    TP4

--- Info/TP4.typ
+++ Info/TP4.typ
@@ -354,6 +354,119 @@ Un résultat est :
     [50000] , [3.28]          
 )
 
+
+== Exercice 4.12
+
+=== 4.12.1
+
+```python
+def get_horse_count(vmin, vmax) ->int:
+    n = -1
+    error_str = ""
+    while n < vmin or n > vmax:
+        n = int(input(f"{error_str}Nombre de cheveaux (entre {vmin} et {vmax}) : "))
+        error_str = "ERREUR - "
+    return n
+```
+
+=== 4.12.2
+
+```python
+def print_odds(c: int):
+    print(f"Vous avez 1 chance sur {c} de trouver le cheval gagnant.")
+    p = 3
+    if c < 8:
+        p = 2
+    print(f"Vous avez {p} chances sur {c} de trouver le cheval placé.")
+    print(f"Vous avez 1 chance sur {c * (c-1) * (c-2)} de trouver le tiercé dans l'ordre.")
+    print(f"Vous avez 1 chance sur {int(c * (c-1) * (c-2)/6)} de trouver le tiercé dans le désordre.")
+
+print_odds(get_horse_count(4,20))
+```
+
+=== 4.12.3
+
+```python
+def get_three_horses(n: int)-> list[int]:
+    r = []
+    k = 3
+    while len(r) < min(k,n):
+        h = random.randint(1,n)
+        if(r.count(h)==0):
+            r.append(h)
+    return r
+```
+
+=== 4.12.4
+
+==== 4.12.4.a Fonctions de saisie
+
+On définit les fonctions de saisie de paramètres suivantes :
+
+```python
+def get_bet_count()->int:
+    n = 0
+    error_str = ""
+    while n !=1 and n!=3:
+        n = int(input(f"{error_str}Sur combien de chevaux pariez-vous ? (1 ou 3) : "))
+        error_str = "ERREUR - "
+    return n
+
+def get_bet(n: int, p: int)-> list[int]:
+    b = []
+    for i in range(1, p+1):
+        b.append(int(input(f"- Cheval {i} (entre 1 et {n}) : ")))
+    return b
+```
+
+==== 4.12.4.b Analyse du résultat
+
+```python
+def print_result(n: int, res: list[int], bet: list[int])-> None:
+
+    if len(bet) == 1:
+        if bet[0] == res[0]:
+            print("Bravo ! Vous avez trouvé un cheval placé et il est gagnant")
+        elif bet[0]==res[1] or (n>=8 and bet[0]==res[2]):
+            print("Bravo ! Vous avez trouvé un cheval placé")
+        else:
+            print("Perdu !")
+    else:
+        won = True
+        order = True
+        h = 0
+        while won and h < 3:
+            won = won and (res.count(bet[h]) > 0)
+            order = order and (bet[h] == res[h])
+            h += 1
+        if won:
+            if order:
+                print("Vous avez trouvé le tiercé dans l'ordre")
+            else:
+                print("Vous avez trouvé le tiercé dans le désordre")
+        else:
+            print("Perdu !")
+```
+
+==== 4.12.4.c Simulation de pari
+
+```python
+def bet_simulation():
+    n = get_horse_count(4, 20)
+    print_odds(n)
+    
+    p = get_bet_count()
+    b = get_bet(n, p)
+    
+    r = get_three_horses(n)
+    print(f"\nLe tiercé gagnant est : {r[0]} - {r[1]} - {r[2]}\n")
+
+    print_result(n, r, b)
+
+bet_simulation()
+```
+
+
 == Exercice 4.13
 
 === 4.13.1
--- Info/code/EXTP/TP/TP4/EX_4.12.py
+++ Info/code/EXTP/TP/TP4/EX_4.12.py
@@ -1,41 +1,88 @@
 import random
 
-def get_horses_count(vmin, vmax) ->int:
+def get_horse_count(vmin, vmax) ->int:
     n = -1
     error_str = ""
     while n< vmin or n > vmax:
-        n = int(input(f"{error_str}Nombre de cheveaux (entre {vmin} et {vmax}) : "))
+        n = int(input(f"{error_str}Nombre de chevaux (entre {vmin} et {vmax}) : "))
         error_str = "ERREUR - "
     return n
 
-def get_three(n: int):
-    horses = []
-    for i in range(1,n+1):
-        horses.append(i)
-    return random.sample(horses, k=3)
 
-def get_tierce(n: int):
+def get_three_horses(n: int)-> list[int]:
     r = []
     k = 3
-    while len(r) < k:
+    while len(r) < min(k,n):
         h = random.randint(1,n)
         if(r.count(h)==0):
             r.append(h)
     return r
-
-        
-    
-def print_odds():
-    c = get_horses_count(4,20)
-    print(f"Vous avez 1 chance sur {c} de trouver le cheval gagnant")
+           
+def print_odds(c: int)-> None:
+    print(f"Vous avez 1 chance sur {c} de trouver le cheval gagnant.")
     p = 3
     if c < 8:
         p = 2
-    print(f"Vous avez {p} chances sur {c} de trouver le cheval placé")
-    print(f"Vous avez 1 chance sur {c * (c-1) * (c-2)} de trouver le tiercé dans l'ordre")
-    print(f"Vous avez 1 chance sur {int(c * (c-1) * (c-2)/6)} de trouver le tiercé dans le désordre")
+    print(f"Vous avez {p} chances sur {c} de trouver le cheval placé.")
+    print(f"Vous avez 1 chance sur {c * (c-1) * (c-2)} de trouver le tiercé dans l'ordre.")
+    print(f"Vous avez 1 chance sur {int(c * (c-1) * (c-2)/6)} de trouver le tiercé dans le désordre.")
 
-    print(get_three(c))
-    print(get_tierce(c))
 
-print_odds()
+def get_bet_count()-> int:
+    n = 0
+    error_str = ""
+    while n !=1 and n!=3:
+        n = int(input(f"{error_str}Sur combien de chevaux pariez-vous ? (1 ou 3) : "))
+        error_str = "ERREUR - "
+    return n
+
+def get_bet(n: int, p: int)-> list[int]:
+    b = []
+    for i in range(1, p+1):
+        b.append(int(input(f"- Cheval {i} (entre 1 et {n}) : ")))
+    return b
+
+
+def print_result(n: int, res: list[int], bet: list[int])-> None:
+
+    if len(bet) == 1:
+        if bet[0] == res[0]:
+            print("Bravo ! Vous avez trouvé un cheval placé et il est gagnant")
+        elif bet[0]==res[1] or (n>=8 and bet[0]==res[2]):
+            print("Bravo ! Vous avez trouvé un cheval placé")
+        else:
+            print("Perdu !")
+    else:
+        won = True
+        order = True
+        h = 0
+        while won and h < 3:
+            won = won and (res.count(bet[h]) > 0)
+            order = order and (bet[h] == res[h])
+            h += 1
+        if won:
+            if order:
+                print("Vous avez trouvé le tiercé dans l'ordre")
+            else:
+                print("Vous avez trouvé le tiercé dans le désordre")
+        else:
+            print("Perdu !")
+
+print_result(4, [1,2,3], [3])
+print_result(7, [1,2,3], [3])
+print_result(8, [1,2,3], [3])
+
+def bet_simulation()-> None:
+    n = get_horse_count(4, 20)
+    print_odds(n)
+    
+    p = get_bet_count()
+    b = get_bet(n, p)
+    
+    r = get_three_horses(n)
+    print(f"\nLe tiercé gagnant est : {r[0]} - {r[1]} - {r[2]}\n")
+
+    print_result(n, r, b)
+
+
+bet_simulation()
