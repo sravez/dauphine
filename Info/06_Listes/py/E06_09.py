c7c4f7411c57b3ba92d09d3596df2e7a30371a68
Serge RAVEZ <serge.ravez@laposte.net>
2025-10-20 21:18:01 +0200

    Update E06_09.py

--- Info/06_Listes/py/E06_09.py
+++ Info/06_Listes/py/E06_09.py
@@ -4,21 +4,21 @@ from functools import reduce
 def TabAlea(n, a, b) -> list[int]:
     return [ randint(a,b) for i in range(0,n) ]
 
-def TabProduit(T) -> int:
-    return reduce(lambda x,y: x*y, T)
-
-def TabProduit_2(T) -> int:
-    p = 1
-    for i in range(0, len(T)):
-        p *= T[i]
-    return p
-
-def TabProduit_3(T) -> int:
+def TabProduit_1(T) -> int:
     p = 1
     i = 0
     while i<len(T) and p != 0 :
         p *= T[i]
         i += 1
+    return p
+
+def TabProduit_2(T) -> int:
+    return reduce(lambda x,y: x*y, T)
+
+def TabProduit_3(T) -> int:
+    p = 1
+    for i in range(0, len(T)):
+        p *= T[i]
     return p
 
 if __name__ == "__main__":
